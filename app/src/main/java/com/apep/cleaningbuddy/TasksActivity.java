c78ab4c9d98572d171e7d933730363915545c2ad
LinkConsolidate <116383894+LinkConsolidate@users.noreply.github.com>
2024-06-12 14:57:23 +0200

    Tasks init overview db data

--- app/src/main/java/com/apep/cleaningbuddy/TasksActivity.java
+++ app/src/main/java/com/apep/cleaningbuddy/TasksActivity.java
@@ -8,19 +8,39 @@ import android.widget.TableRow;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class TasksActivity extends BaseActivity {
+import com.apep.cleaningbuddy.adapters.OpenTaskAdapter;
+import com.apep.cleaningbuddy.adapters.RoomAdapter;
+import com.apep.cleaningbuddy.adapters.TaskAdapter;
+import com.apep.cleaningbuddy.interfaces.OnOpenTaskClickListener;
+import com.apep.cleaningbuddy.interfaces.OnTaskClickListener;
+import com.apep.cleaningbuddy.models.Room;
+import com.apep.cleaningbuddy.models.Task;
+
+import java.util.List;
+
+public class TasksActivity extends BaseActivity implements OnOpenTaskClickListener {
 
     private Button yourTasksButton;
     private Button openTasksButton;
     private Button allTasksButton;
     private Button addButton;
+    private RecyclerView recyclerView;
+    private List<Task> tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
         addNavbarListeners();
+
+        tasks = Task.getAll(this);
+        recyclerView = findViewById(R.id.open_tasks_list_rv);
+        OpenTaskAdapter adapter = new OpenTaskAdapter(tasks,this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         yourTasksButton = findViewById(R.id.Tasks_yourTasks_btn_id);
         openTasksButton = findViewById(R.id.Tasks_openTasks_btn_id);
@@ -63,19 +83,8 @@ public class TasksActivity extends BaseActivity {
         });
     }
 
-    private void setupTaskRowClickListener(int rowId, final String taskName, final String interval, final String room, final String assignedUser, final String description) {
-        TableRow taskRow = findViewById(rowId);
-        taskRow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(TasksActivity.this, TaskDetailsActivity.class);
-                intent.putExtra("TASK_NAME", taskName);
-                intent.putExtra("INTERVAL", interval);
-                intent.putExtra("ROOM", room);
-                intent.putExtra("ASSIGNED_USER", assignedUser);
-                intent.putExtra("DESCRIPTION", description);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public void onTaskClick(Task task) {
+        // TODO: afvinken
     }
 }
